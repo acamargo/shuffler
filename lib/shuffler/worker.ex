6c3ca65c95339f9cbbabc3d209b5d36f95f4cef9
André Camargo <andre@portaiseletronicos.com.br>
2016-06-16 20:39:26 -0300

    Parallel version of Shuffler.Worker.run

--- lib/shuffler/worker.ex
+++ lib/shuffler/worker.ex
@@ -28,6 +28,43 @@ defmodule Shuffler.Worker do
     _words(words, [], dictionary)
   end
 
+  @doc """
+  Parallel version of Shuffler.Worker.run
+
+  It creates a new process to shuffle each word provided.
+
+  ## Examples
+
+    iex> Shuffler.Worker.prun(["a"])
+    ["a"]
+
+    iex> Shuffler.Worker.prun(["a"], %{"a" => ["a","@","4"]})
+    ["a", "@", "4"]
+
+    iex> Shuffler.Worker.prun(["b"], %{"a" => ["@","4"]})
+    ["b"]
+
+    iex> Shuffler.Worker.prun(["arma"], %{"a" => ["a","@","4"]})
+    ["arma", "arm@", "arm4", "@rma", "@rm@", "@rm4", "4rma", "4rm@", "4rm4"]
+
+    iex> Shuffler.Worker.prun(["carro"], %{"c"=>["c","k"], "a"=>["a","@","4"], "o"=>["o","0"]})
+    ["carro", "carr0", "c@rro", "c@rr0", "c4rro", "c4rr0", "karro", "karr0", "k@rro", "k@rr0", "k4rro", "k4rr0"]
+
+    iex> Shuffler.Worker.prun(["arma", "carro"], %{"c"=>["c","k"], "a"=>["a","@","4"], "o"=>["o","0"]})
+    ["arma", "arm@", "arm4", "@rma", "@rm@", "@rm4", "4rma", "4rm@", "4rm4", "carro", "carr0", "c@rro", "c@rr0", "c4rro", "c4rr0", "karro", "karr0", "k@rro", "k@rr0", "k4rro", "k4rr0"]
+  """
+  def prun(words, dictionary \\ %{}) do
+    me = self
+    words
+    |> Enum.map(fn (word) ->
+      spawn_link fn -> (send me, { self, _words([word], [], dictionary) }) end
+    end)
+    |> Enum.map(fn (pid) ->
+      receive do { ^pid, result } -> result end
+    end)
+    |> List.flatten
+  end
+
   defp _words([], acc, _dictionary), do: acc
   defp _words([head|tail], acc, dictionary) do
     _characters(String.codepoints(head), "", dictionary) ++
